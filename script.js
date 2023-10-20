0ce9f82cb17615408d5a992b351d27b3597c9f7b
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-20 15:54:42 -0600

    Add reset and user prompt option

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-
-//Fill box with boxes
-const boxSideCount = 16
-const boxCount = boxSideCount * boxSideCount
-console.log(boxCount)
 const container = document.querySelector('.container')
 console.log(container)
+let userChoice = 0
+//reset and send prompt
+const resetButton = document.querySelector('button')
+resetButton.onclick = function(){
+    while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild)
+    }
+    let userChoice = prompt ('How many squares per side (<=100) would you like?')
+        if (userChoice > 100) {
+            let userChoice = prompt('Please enter a number of 100 or less')
+        }
+    let boxSideCount = userChoice
+    let boxCount = boxSideCount * boxSideCount
+    console.log(boxCount)
+    let widHigh = 960 / boxSideCount
+    console.log(widHigh)
+     
+    
+    for ( let i = 0; i < boxCount; i++) {
+            let box = document.createElement('div')
+            box.classList.add('box');
+            box.style.minWidth = widHigh + 'px';
+            box.style.minHeight = widHigh + 'px';
+            container.appendChild(box)
+    }
+            
+}
+
+
+//Fill box with boxes
+/* let boxSideCount = userChoice
+let boxCount = boxSideCount * boxSideCount
+console.log(boxCount)
 let widHigh = 960 / boxSideCount
 console.log(widHigh)
 
@@ -15,9 +43,8 @@ for ( let i = 0; i < boxCount; i++) {
     box.style.minHeight = widHigh + 'px';
     container.appendChild(box)
 }
-
+ */
 //Hover to paint
-
 onmouseover = (e) => {
     if (e.target.classList == 'box') {
     e.target.classList.add('painted')}
