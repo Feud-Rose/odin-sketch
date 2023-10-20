bf8e55bd94ccbc0ea65156b892a7f195d3cc2258
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-20 15:56:43 -0600

    Remove unused script and comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container')
 console.log(container)
 let userChoice = 0
-//reset and send prompt
+
+
 const resetButton = document.querySelector('button')
 resetButton.onclick = function(){
     while (container.hasChildNodes()) {
@@ -28,23 +29,6 @@ resetButton.onclick = function(){
             
 }
 
-
-//Fill box with boxes
-/* let boxSideCount = userChoice
-let boxCount = boxSideCount * boxSideCount
-console.log(boxCount)
-let widHigh = 960 / boxSideCount
-console.log(widHigh)
-
-for ( let i = 0; i < boxCount; i++) {
-    let box = document.createElement('div')
-    box.classList.add('box');
-    box.style.minWidth = widHigh + 'px';
-    box.style.minHeight = widHigh + 'px';
-    container.appendChild(box)
-}
- */
-//Hover to paint
 onmouseover = (e) => {
     if (e.target.classList == 'box') {
     e.target.classList.add('painted')}
